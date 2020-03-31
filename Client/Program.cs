a36f928626ecfc0c7f1678ad0f1aff4f2753be23
efimenkoda <dimas86@bk.ru>
2020-03-31 21:42:40 +0500

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            UdpClient client = new UdpClient("127.0.0.1", 8000);
+            try
+            {
+                while (true)
+                {
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    client.Send(data, data.Length);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
---
+++ UDP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient recieve = new UdpClient(8000);
+            IPEndPoint iPEndPoint = null;
+
+            try
+            {
+
+                while (true)
+                {
+                    byte[] data = recieve.Receive(ref iPEndPoint);
+                    string message = Encoding.UTF8.GetString(data);
+                    Console.WriteLine(message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                recieve.Close();
+            }
+        }
+    }
+}
